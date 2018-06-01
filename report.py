e0e26ddbed4a2e424905f27d6056e3e3ff969f00
vagrant <vagrant@vagrant.vm>
2018-06-01 20:32:08 +0000

    finish queries 2 and 3

--- report.py
+++ report.py
@@ -4,7 +4,49 @@ db = psycopg2.connect("dbname=news")
 
 cursor = db.cursor()
 
-cursor.execute("select path, count(path) as num from log where path like '/article/%' group by path order by num desc limit 3;")
+cursor.execute("""
+select title, count(path) as num
+from log, articles
+where path like '%' || articles.slug || '%' and status = '200 OK'
+group by title
+order by num desc 
+limit 3;
+""")
+
+results = cursor.fetchall()
+
+print (results)
+
+cursor.execute("""
+select name, count(path) as num 
+from articles, authors, log
+where authors.id = articles.author and path like '%' || articles.slug || '%' and status = '200 OK'
+group by name 
+order by count(path) desc 
+limit 3;
+""")
+
+results = cursor.fetchall()
+
+print (results)
+
+cursor.execute("""
+create view error_view as 
+select date(time) as f_date, sum(case when
+    substring(status, 0, 4)::int >= 400
+    then 1
+    else 0
+    end)::decimal / (count(log.status)) as f_per
+from log
+group by date(time);
+
+select to_char(f_date, 'Month DD YYYY'), round(f_per * 100, 1) || '%'
+from error_view
+group by f_date, f_per
+having f_per > 0.01
+""")
+
+
 
 results = cursor.fetchall()
 
